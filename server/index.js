40b8557409550f93d1a781ea8545f65dfc54b11a
yaweiz <yawei19970710@outlook.com>
2020-05-04 10:42:54 -0400

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-// generate a express application
+// generate a express application, and create the first route handler
 
-// import express library
+// import express library, common js modules
 const express = require('express');
-const app = express();
+const app = express();   // create express application
 
 // route handler
 app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;  // Listen for heroku port
+app.listen(PORT);
