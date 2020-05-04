d4241ad8ce779450461d19575060ef77139e5dd6
yaweiz <yawei19970710@outlook.com>
2020-05-04 11:25:48 -0400

    changes greeting

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();   // create express application
 
 // route handler
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'buddy'});
 });
 
 const PORT = process.env.PORT || 5000;  // Listen for heroku port
