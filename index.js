46f7c119a84b45fbc4f0b42f7600980ef3835f71
yaweiz <yawei19970710@outlook.com>
2020-05-06 20:22:31 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -3,15 +3,29 @@
 // import express library, common js modules
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');    // get access to cookies
+const passport = require('passport');          // tell passport to keep track of user session
 const keys = require('./config/keys');
+require('./models/User');                  // define User model here, the order of requires really matters
 require('./services/passport');
-require('./models/User');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();   // create express application
 
-require('./routes/authRoutes')(app);
+// Authentication
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]     // choose from multiple keys
+    })
+);
 
-mongoose.connect(keys.mongoURI);
+app.use(passport.initialize());
+app.use(passport.session());      // tell passport to use cookies to authenticate
+
+
+require('./routes/authRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;  // Listen for heroku port
