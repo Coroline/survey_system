45a1a09081af6a53f5afa2d06efcc8498537f81c
yaweiz <yawei19970710@outlook.com>
2020-05-12 15:36:24 -0400

    finish dashboard

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_USER } from '../actions/types';
 
 export default function(state = null, action) {      // determine whether or not the user login
-    console.log(action);
+    // console.log(action);
     switch (action.type) {
         case FETCH_USER:
             return action.payload || false;
